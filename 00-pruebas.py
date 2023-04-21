c693c3ca7c653fe2cb0cfbc25893f326de83a4eb
chenindulce <122371486+chenindulce@users.noreply.github.com>
2023-04-21 11:03:07 -0300

    Add files via upload

---
+++ 00-pruebas.py
@@ -0,0 +1,18 @@
+#ejercicio tema 3
+
+def suma(a,b,c):
+
+    print(a+b+c)
+
+suma(1, 2, 3)
+
+
+
+class Coche:   
+
+    def agregar_puerta():
+        puertas = 3
+        puertas += 1
+        print('cantidad de puertas:' , puertas)
+
+    agregar_puerta()
